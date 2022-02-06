9e1f13820522787b688aef38b26f97d4b852e086
adrian154 <94568a@gmail.com>
2022-02-05 20:57:10 -0800

    s

--- site/main.js
+++ site/main.js
@@ -34,6 +34,44 @@ for(const course of COURSES) {
     });
 
     // add
+    course.element = entry.querySelector(".course-list-entry");
     courseList.append(entry);
 
 }
+
+// update filters
+const schoolFilter = document.getElementById("filter-school"),
+      gradeLevelFilter = document.getElementById("filter-grade-level"),
+      subjectAreaFilter = document.getElementById("filter-subject-area"),
+      ucReqsFilter = document.getElementById("filter-uc-req");
+
+const refreshFilters = () => {
+    
+    const school = schoolFilter.querySelector(":checked").value;
+    const grades = [...gradeLevelFilter.querySelectorAll(":checked")].map(node => node.value);
+    const subjects = [...subjectAreaFilter.querySelectorAll(":checked")].map(node => node.value);
+    const ucReqs = [...ucReqsFilter.querySelectorAll(":checked")].map(node => node.value);
+
+    let courses = COURSES.filter(course => course.schools.includes(school));
+    
+    if(grades.length > 0) courses = courses.filter(course => {
+        for(const grade of grades) {
+            if(course.gradesAvailable.includes(Number(grade))) return true;
+        }
+    });
+
+    console.log(subjects);
+    if(subjects.length > 0) courses = courses.filter(course => subjects.includes(course.subject));
+    if(ucReqs.length > 0) courses = courses.filter(course => ucReqs.includes(course.ucCategory));
+
+    for(const course of COURSES) {
+        course.element.style.display = courses.includes(course) ? "" : "none";
+    }
+
+};
+
+// attach event listeners for filters
+schoolFilter.querySelectorAll("input").forEach(node => node.addEventListener("input", refreshFilters));
+gradeLevelFilter.querySelectorAll("input").forEach(node => node.addEventListener("input", refreshFilters));
+subjectAreaFilter.querySelectorAll("input").forEach(node => node.addEventListener("input", refreshFilters));
+ucReqsFilter.querySelectorAll("input").forEach(node => node.addEventListener("input", refreshFilters));
